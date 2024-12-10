d19527a3d6acae7985a36a5e1d711457fcf741f7
Stephen Royle <s.j.royle@warwick.ac.uk>
2024-12-10 12:17:53 +0000

    Feat: switch to strings for column definitions, switchable mean-median for markers

--- R/get_sp_colours.R
+++ R/get_sp_colours.R
@@ -1,62 +1,68 @@
-#' SuperPlot Colour Palette Selection
-#'
-#' @param n integer number of colours to return
-#' @param scheme character name, colour scheme to use
-#'  Choose from one of four palettes from Paul Tol ("tol_bright", "tol_vibrant",
-#'  "tol_muted", "tol_light"), or Color Universal Design ("cud"). Or a vector of
-#'  colours in hex format, e.g. c("#FF0000", "#00FF00", "#0000FF").
-#'
-#' @return character vector of colours
-#' @export
-#'
-#' @examples
-#' get_sp_colours(3, "tol_bright")
+#" SuperPlot Colour Palette Selection
+#"
+#" @param n integer number of colours to return
+#" @param scheme character name, colour scheme to use
+#"  Choose from one of four palettes from Paul Tol ("tol_bright", "tol_vibrant",
+#"  "tol_muted", "tol_light"), or Color Universal Design ("cud"). Or a vector of
+#"  colours, e.g. c("#FF0000", "green", "#0000FF").
+#"
+#" @return character vector of colours
+#" @export
+#"
+#" @examples
+#" get_sp_colours(3, "tol_bright")
 get_sp_colours <- function(n, scheme) {
   colours <- NULL
 
   # check if scheme is a vector of colours and return it if so
+  # will result in an error if user enters non-colours as elements
   if (is.vector(scheme) && length(scheme) > 1) {
     if (length(scheme) < n) {
       colours <- rep(scheme, length.out = n)
     } else {
-    colours <- scheme[1:n]
+      colours <- scheme[1:n]
     }
     return(colours)
   }
 
-  tol_bright <- c('#4477AA', '#EE6677', '#228833', '#CCBB44', '#66CCEE', '#AA3377', '#BBBBBB')
-  tol_vibrant <-  c('#EE7733', '#0077BB', '#33BBEE', '#EE3377', '#CC3311', '#009988', '#BBBBBB')
-  tol_muted <- c('#CC6677', '#332288', '#DDCC77', '#117733', '#88CCEE', '#882255', '#44AA99', '#999933', '#AA4499')
-  tol_light <- c('#77AADD', '#EE8866', '#EEDD88', '#FFAABB', '#99DDFF', '#44BB99', '#BBCC33', '#AAAA00', '#DDDDDD')
-  cud <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7", "#000000")
+  tol_bright <- c("#4477AA", "#EE6677", "#228833",
+                  "#CCBB44", "#66CCEE", "#AA3377",
+                  "#BBBBBB")
+  tol_vibrant <-  c("#EE7733", "#0077BB", "#33BBEE",
+                    "#EE3377", "#CC3311", "#009988",
+                    "#BBBBBB")
+  tol_muted <- c("#CC6677", "#332288", "#DDCC77",
+                 "#117733", "#88CCEE", "#882255",
+                 "#44AA99", "#999933", "#AA4499")
+  tol_light <- c("#77AADD", "#EE8866", "#EEDD88",
+                 "#FFAABB", "#99DDFF", "#44BB99",
+                 "#BBCC33", "#AAAA00", "#DDDDDD")
+  cud <- c("#E69F00", "#56B4E9", "#009E73",
+           "#F0E442", "#0072B2", "#D55E00",
+           "#CC79A7", "#000000")
 
   # get the colour scheme
-  if(scheme == "tol_bright") {
-    if (n == 1) {
-      x <- 1
-    } else if (n == 2) {
-      x <- c(1, 2)
-    } else if (n == 3) {
-      x <- c(1, 4, 2)
-    } else if (n == 4) {
-      x <- c(1, 3, 4, 2)
-    } else if (n == 5) {
-      x <- c(1, 5, 3, 4, 2)
-    } else if (n == 6) {
-      x <- c(1, 5, 3, 4, 2, 6)
+  if (scheme == "tol_bright") {
+    if (n < 7) {
+      selection <-  list(c(1), c(1, 2), c(1, 4, 2), c(1, 3, 4, 2),
+                         c(1, 5, 3, 4, 2), c(1, 5, 3, 4, 2, 6))
+      x <- selection[[n]]
     } else {
-      x <- rep(c(, 5, 3, 4, 2, 6, 7), length.out = n)
+      x <- rep(c(1, 5, 3, 4, 2, 6, 7), length.out = n)
     }
     colours <- tol_bright[x]
-  } else if(scheme == "tol_vibrant") {
+  } else if (scheme == "tol_vibrant") {
     x <- rep(c(1, 2, 3, 4, 5, 6, 7), length.out = n)
     colours <- tol_vibrant[x]
-  } else if(scheme == "tol_muted") {
+  } else if (scheme == "tol_muted") {
     x <- rep(c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), length.out = n)
     colours <- tol_muted[x]
-  } else if(scheme == "tol_light") {
+  } else if (scheme == "tol_light") {
     x <- rep(c(1, 2, 3, 4, 5, 6, 7, 8, 9), length.out = n)
     colours <- tol_light[x]
+  } else if (scheme == "cud") {
+    x <- rep(c(1, 2, 3, 4, 5, 6, 7, 8), length.out = n)
+    colours <- cud[x]
   } else {
     stop("Invalid colour scheme. Choose a palette or use your own vector of
          colours.")
--- R/superplot.R
+++ R/superplot.R
@@ -1,12 +1,15 @@
 #' Make a SuperPlot
 #'
 #' @param df data frame with at least three columns: meas, cond, repl
-#' @param meas name of column with measurement (e.g. intensity or speed)
-#' @param cond name of column with condition (e.g. Control, WT)
-#' @param repl name of column with replicate (e.g. unique experiment ids)
+#' @param meas character name of column with measurement (e.g. intensity)
+#' @param cond character name of column with condition (e.g. Control, WT)
+#' @param repl character name of column with replicate (e.g. unique experiment
+#' identifiers)
 #' @param pal name of colour palette to use (default is "tol_bright")
 #' @param xlab string for x label (default is empty)
 #' @param ylab string for y label (default is "Measurement")
+#' @param rep_summary string for summary statistic to use for replicates, select
+#' ("rep_mean" or "rep_median")
 #' @param gg ggplot object to add to (default is NULL)
 #'
 #' @return ggplot object
@@ -14,37 +17,43 @@
 #' @import dplyr
 #' @import ggforce
 #' @import cowplot
-#' @importFrom stats sd
+#' @importFrom stats sd median
 #'
 #' @export
 #'
 #' @examples
-#' superplot(lord_jcb, Speed, Treatment, Replicate, ylab = "Speed (um/min)")
+#' superplot(lord_jcb,
+#' "Speed", "Treatment", "Replicate", ylab = "Speed (um/min)")
 #'
 superplot <- function(df,
                       meas, cond, repl,
                       pal = "tol_bright",
                       xlab = "", ylab = "Measurement",
-                      gg = NULL){
-
-  ncond <- nrepl <- Mean <- SD <- NULL
+                      rep_summary = "rep_mean",
+                      gg = NULL) {
+  ncond <- nrepl <- NULL
+  rep_mean <- rep_sd <- rep_sem <- rep_ci <- NULL
 
   # if the repl column is not character, convert it
-  if (!is.character(df[[deparse(substitute(repl))]])) {
-    df[[deparse(substitute(repl))]] <-
-      as.character(df[[deparse(substitute(repl))]])
+  if (!is.character(df[[repl]])) {
+    df[[repl]] <- as.character(df[[repl]])
   }
 
   # how many unique values in cond and repl?
-  ncond <- length(unique(df[[deparse(substitute(cond))]]))
-  nrepl <- length(unique(df[[deparse(substitute(repl))]]))
+  ncond <- df %>% pull(!!sym(cond)) %>% unique() %>% length()
+  nrepl <- df %>% pull(!!sym(repl)) %>% unique() %>% length()
 
   # calculate summary statistics
   summary_df <- df %>%
-    group_by({{cond}}, {{repl}}) %>%
-    summarise(Mean = mean({{meas}}, na.rm = TRUE),
-              SD = sd({{meas}}, na.rm = TRUE))
-  # generate a warning is NROW of summary_df doesn't equal the product of
+    group_by(!!sym(cond), !!sym(repl)) %>%
+    summarise(
+      rep_mean = mean(!!sym(meas), na.rm = TRUE),
+      rep_median = median(!!sym(meas), na.rm = TRUE),
+      rep_sd = sd(!!sym(meas), na.rm = TRUE),
+      rep_sem = sd(!!sym(meas), na.rm = TRUE) / sqrt(n()),
+      rep_ci = 1.96 * sd(!!sym(meas), na.rm = TRUE) / sqrt(n())
+    )
+  # generate a warning if NROW of summary_df doesn't equal the product of
   # unique values in cond and repl
   if (nrow(summary_df) != ncond * nrepl) {
     warning("Summary statistics were not calculated for all combinations of
@@ -54,7 +63,7 @@ superplot <- function(df,
   sp_colours <- get_sp_colours(nrepl, pal)
 
   # we may have an existing ggplot object to add to
-  if(is.null(gg)){
+  if (is.null(gg)) {
     p <- ggplot()
   } else {
     p <- gg
@@ -62,14 +71,18 @@ superplot <- function(df,
 
   # make superplot
   p <- p +
-    geom_sina(data = df,
-              aes(x = {{cond}}, y = {{meas}}, colour = {{repl}}),
-              shape = 16, alpha = 0.5, position = "auto",
-              size = 0.8, maxwidth = 0.3) +
-    geom_point(data = summary_df,
-               aes(x = {{cond}}, y = Mean, fill = {{repl}}),
-               shape = 22, size = 1.5, stroke = 0.5, alpha = 0.7) +
-    scale_color_manual(values = sp_colours) +
+    geom_sina(
+      data = df,
+      aes(x = !!sym(cond), y = !!sym(meas), colour = !!sym(repl)),
+      shape = 16, alpha = 0.5, position = "auto",
+      size = 0.8, maxwidth = 0.3
+    )
+  p <- p + geom_point(
+      data = summary_df,
+      aes(x = !!sym(cond), y = !!sym(rep_summary), fill = !!sym(repl)),
+      shape = 22, size = 1.5, stroke = 0.5, alpha = 0.7
+    )
+  p <-  p + scale_color_manual(values = sp_colours) +
     scale_fill_manual(values = sp_colours) +
     labs(x = xlab, y = ylab) +
     lims(y = c(0, NA)) +
