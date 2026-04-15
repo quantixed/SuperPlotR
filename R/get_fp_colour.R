fb42c3cf336b3657a39e2375bb269b087d06f124
Stephen Royle <s.j.royle@warwick.ac.uk>
2026-04-15 15:10:01 +0100

    Feat: expand colour/color usage

--- R/get_fp_colour.R
+++ R/get_fp_colour.R
@@ -1,7 +1,14 @@
-#' FlatPlot Colour Selection (Multiple Colors)
+#' FlatPlot Colour Selection (Multiple Colours)
 #'
-#' @param keys character vector of color names/codes, can be "rl_green", "rl_red", "rl_blue",
-#' "rl_yellow", "rl_purple", "rl_orange", "rl_magenta", hex colours, or valid R color names
+#' This function takes a character vector of color names/codes and returns a
+#' character vector of hex colours. It uses the `get_fp_colour` function to
+#' convert each color name/code to its corresponding hex colour. The input can
+#' include predefined color names like "rl_green", "rl_red", etc., as well as
+#' hex color codes or valid R color names. Use of `get_fp_colors` is valid too.
+#'
+#' @param keys character vector of color names/codes, can be "rl_green",
+#'   "rl_red", "rl_blue", "rl_yellow", "rl_purple", "rl_orange", "rl_magenta",
+#'   "rl_inv", or hex colours, or valid R color names
 #'
 #' @return character vector of hex colours
 #' @export
@@ -13,11 +20,22 @@ get_fp_colours <- function(keys) {
   # Apply get_fp_colour to each element in the vector
   sapply(keys, get_fp_colour, USE.NAMES = FALSE)
 }
+#' @rdname get_fp_colours
+#' @examples get_fp_colors("rl_green")
+#' @export
+get_fp_colors <- get_fp_colours
 
-#' FlatPlot Colour Selection (Single Color)
+#' FlatPlot Colour Selection (Single Colour)
+#'
+#' This function takes a single character name/code and returns the
+#' corresponding hex colour. It checks if the input is a valid hex colour code
+#' or a valid R color name, and if not, it uses a predefined mapping for
+#' specific color names like "rl_green", "rl_red", etc. If the input is not
+#' valid, it will return NULL. Use of `get_fp_color` is valid too.
 #'
 #' @param key character name, can be "rl_green", "rl_red", "rl_blue",
-#' "rl_yellow", "rl_purple", "rl_orange", "rl_magenta", or a hex colour
+#'   "rl_yellow", "rl_purple", "rl_orange", "rl_magenta", "rl_inv", or a hex
+#'   colour, or a valid R color name
 #'
 #' @return character of hex colour
 #' @importFrom grDevices colors
@@ -44,6 +62,11 @@ get_fp_colour <- function(key) {
          "rl_yellow" = "#ffc620",
          "rl_purple" = "#64318e",
          "rl_orange" = "#f59331",
-         "rl_magenta" = "#da70d6")
+         "rl_magenta" = "#da70d6",
+         "rl_inv" = "#ab66f0")
   return(colour)
 }
+#' @rdname get_fp_colour
+#' @examples get_fp_color("rl_green")
+#' @export
+get_fp_color <- get_fp_colour
--- R/get_sp_colours.R
+++ R/get_sp_colours.R
@@ -1,5 +1,15 @@
 #' SuperPlot Colour Palette Selection
 #'
+#' This function takes an integer `n` and a character `scheme` and returns a
+#' character vector of `n` colours from the specified colour scheme. The
+#' function supports four colour schemes from Paul Tol ("tol_bright",
+#' "tol_vibrant", "tol_muted", "tol_light") and Color Universal Design ("cud"
+#' scheme). If the `scheme` argument is a vector of colours, the function will
+#' return the first `n` colours from that vector, repeating the vector if it has
+#' fewer than `n` colours. If the `scheme` argument is not a valid colour scheme
+#' or a vector of colours, the function will throw an error. Use of
+#' `get_sp_colors()` is valid too.
+#'
 #' @param n integer number of colours to return
 #' @param scheme character name, colour scheme to use
 #'  Choose from one of four palettes from Paul Tol ("tol_bright", "tol_vibrant",
@@ -69,3 +79,7 @@ get_sp_colours <- function(n, scheme) {
   }
   return(colours)
 }
+#' @rdname get_sp_colours
+#' @examples get_sp_colors(3, "tol_bright")
+#' @export
+get_sp_colors <- get_sp_colours
